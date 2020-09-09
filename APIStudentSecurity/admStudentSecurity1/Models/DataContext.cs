6c95198e53f05bf46eb0632b06b29e6df249de83
Brinna Francesca Gorena Llado <a2019119323@estudiantes.upsa.edu.bo>
2020-09-09 10:13:58 -0400

    Se agrego el proyecto de administracion MVC.

---
+++ APIStudentSecurity/admStudentSecurity1/Models/DataContext.cs
@@ -0,0 +1,13 @@
+﻿namespace admStudentSecurity1.Models
+{
+    using System.Data.Entity;
+    public class DataContext : DbContext
+    {
+        public DataContext() : base("DefaultConnection") //constructor
+        {
+
+        }
+
+        public System.Data.Entity.DbSet<admStudentSecurity1.Models.Student> Students { get; set; }
+    }
+}
---
+++ APIStudentSecurity/admStudentSecurity1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(admStudentSecurity1.Startup))]
+namespace admStudentSecurity1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
