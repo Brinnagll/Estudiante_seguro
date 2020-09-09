538751d9bbbaa90ad83e9927588c3d4fd4f3adbf
Brinna Francesca Gorena Llado <a2019119323@estudiantes.upsa.edu.bo>
2020-09-09 14:19:33 -0400

    Se creo el Unit Test del API Web Students.

---
+++ APIStudentSecurity/StudentSecurity1.Tests/Controllers/UnitTestAPIStudent.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using APIStudentSecurity.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace StudentSecurity1.Tests.Controllers
+{
+    [TestClass]
+    public class UnitTestAPIStudent
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //Arrange
+            StudentsController studentsController = new StudentsController(); //objeto 
+
+            //Act
+            var listaEstudiantes = studentsController.GetStudents();
+
+
+            //Assert
+            Assert.IsNotNull(listaEstudiantes);
+        }
+    }
+}
